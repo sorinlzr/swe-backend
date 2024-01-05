b8d3017c0ceecd205b6eaf4cbbda473f2d5fd64e
sorin.lzr <soso.laz@gmail.com>
2024-01-06 00:39:46 +0100

    BUZZ-4: add userController and userRouter.
    TODO: add userController methods for updating and getting a user, update password, follow and unfollow user, add and remove favorite

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 import { connectToDatabase } from './config/dbconnection';
+import cors from "cors";
+import bodyParser from 'body-parser';
 import express from "express";
 import 'dotenv/config'
+import userRouter from './routers/userRouter';
 
 const port = process.env.SWE_BACKEND_PORT || 5000;
+const corsOptions = {
+    origin: `http://localhost:${process.env.SWE_FRONTEND_PORT}`,
+};
 
 const app = express();
+app.use(cors(corsOptions));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/", function (request: any, response: any) {
     response.send("Hello World!")
 })
+
+app.use("/api/users", userRouter);
 
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
