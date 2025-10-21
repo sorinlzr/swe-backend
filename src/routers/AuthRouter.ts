fd6d726ac1b1dca1b7898f3350b38e4ac8df1e85
sorinlzr <soso.laz@gmail.com>
2025-10-21 23:32:55 +0200

    remove unused export

--- src/routers/AuthRouter.ts
+++ src/routers/AuthRouter.ts
@@ -4,7 +4,7 @@ dotenv.config({ path: './.env' });
 import express from 'express';
 import { login, logout } from '../controllers/AuthController';
 
-export const authRouter = express.Router();
+const authRouter = express.Router();
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
 
