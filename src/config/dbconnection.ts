fd51a6267e1ec030a7d8f551123fea2e3cba761e
sorinlzr <soso.laz@gmail.com>
2025-10-21 23:31:42 +0200

    refactor dbconnection service

--- src/config/dbconnection.ts
+++ src/config/dbconnection.ts
@@ -2,8 +2,15 @@ import 'dotenv/config'
 import mongoose from 'mongoose';
 import { Category }  from '../models/Category';
 import { CategoryModel } from '../models/Category';
+import createLogger from '../utils/logger';
 
-const mongodbURI: string = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`;
+const logger = createLogger('DBConnectionService');
+
+const mongoHost = process.env.MONGO_HOST || 'mongo';
+const mongoPort = process.env.MONGO_PORT || '27017';
+const mongoDbName = process.env.MONGO_DB_NAME || 'buzz_db';
+
+const mongodbURI: string = `mongodb://${mongoHost}:${mongoPort}/${mongoDbName}`;
 
 export const connectToDatabase = async (): Promise<void> => {
     try {
@@ -14,21 +21,21 @@ export const connectToDatabase = async (): Promise<void> => {
             authSource: 'admin'
         });
 
-        console.log('MongoDB is Connected...');
+        logger.info('MongoDB is Connected...');
     } catch (err: any) {
-        console.log("There was an issue connecting to MongoDB");
-        console.error(err.message);
+        logger.error("There was an issue connecting to MongoDB");
+        logger.error(err.message);
     }
 };
 
 export const initializeCategories = async(): Promise<void> => {
     const categoryValues = Object.values(Category).filter(value => typeof value === 'string');
     for (const category of categoryValues) {
-      const existingCategory = await CategoryModel.findOne({ name: category });
-      if (!existingCategory) {
-        const newCategory = new CategoryModel({ name: category });
-        await newCategory.save();
-        console.log('Category created: ', category);
-      }
+        const existingCategory = await CategoryModel.findOne({ name: category });
+        if (!existingCategory) {
+            const newCategory = new CategoryModel({ name: category });
+            await newCategory.save();
+            logger.info('Category created: ', category);
+        }
     }
 }
